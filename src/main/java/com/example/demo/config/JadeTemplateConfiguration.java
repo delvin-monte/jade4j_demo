b657034a6ccf94023fac9e8a64e39ad795c3c618
Bertrand Tabayoyong Tano <btano@google.com>
2018-10-30 15:08:16 -0700

    Removed unused import.

--- src/main/java/com/example/demo/config/JadeTemplateConfiguration.java
+++ src/main/java/com/example/demo/config/JadeTemplateConfiguration.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.Reader;
